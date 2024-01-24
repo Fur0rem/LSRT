4465a735479311c9cce1f5ba11e185974908b084
21105419 <pierre.baumann@etu.sorbonne-universite.fr>
2024-01-24 10:26:02 +0100

    Testing again

--- src/main.c
+++ src/main.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 
+
+
 int main() {
-	printf("Start of program.\n");
-	printf("Hello, world!\n");
-	printf("I need to add a new feature.\n");
+printf("Start of program.\n");printf("Hello, world!\n");    printf("I need to add a new feature.\n");
+
+
+
+
+
+
+
 	printf("End of program.\n");
-	return 0;
-}
+	return 0;}
