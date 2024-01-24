cecd16b9cefc40f02fa5fb766c7b106da248b5f1
21105419 <pierre.baumann@etu.sorbonne-universite.fr>
2024-01-24 13:35:46 +0100

    Making sure unit tests work

--- run_tests.sh
+++ run_tests.sh
@@ -1,9 +1,10 @@
 error=0
 for test in test/bin/*; do
-failure=$(valgrind --leak-check=full --error-exitcode=1 --show-leak-kinds=all --errors-for-leak-kinds=all $test 2>&1 | sed -i 's/ ==/\n==/g')
+valgrind --leak-check=full --error-exitcode=1 --show-leak-kinds=all --errors-for-leak-kinds=all $test 2>tmp
+failure=$(sed -i 's/ ==/\n==/g' tmp) 
 if [ $? -ne 0 ]; then
 	error=1
-	echo $failure
+	echo "failure $failure"
 	echo "Test $test failed"
 else
 	echo "Test $test passed"
