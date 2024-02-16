5423c9c89fe00948cf029f5280931fb64bdd86cd
k-avi <lkingnavii@gmail.com>
2024-02-16 13:43:17 +0100

    fixed minor issues in deleted links and main

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <stdint.h>
 #include <stdio.h>
 #include "src/algo.h"
 #include "src/common.h"
@@ -8,6 +9,7 @@ int main(int argc, char ** argv) {
 
 	uint8_t opt_mask = 0 ;
 	FILE * flux = stdout ;
+	uint8_t opt_shift = 0 ; 
 
 	char chr;
     while ((chr = getopt(argc, argv, "hf:")) != -1) {
@@ -15,13 +17,16 @@ int main(int argc, char ** argv) {
         switch (chr) {
         case 'h': //help
             opt_mask |= 1; 
+			opt_shift++;
             break;
         case 'f' : //output file
 			opt_mask |= 1<<1; 
-			flux = fopen(optarg,"a");
+			opt_shift+=2;
+			flux = fopen(optarg,"w");
+			break;
 
         case '?':    
-            fprintf(stderr, "Unknown option character `\\x%x'.", optopt);          
+            fprintf(stderr, "Unknown option character %c.", optopt);          
             break;
         default:
             abort();
@@ -35,8 +40,8 @@ int main(int argc, char ** argv) {
 
 	//parses link stream from files
 	declare_link_stream(lks);
-	FILE * fgraph = fopen(argv[1], "r");
-	FILE * fattack = fopen(argv[2] , "r");
+	FILE * fgraph = fopen(argv[1+opt_shift], "r");
+	FILE * fattack = fopen(argv[2+opt_shift] , "r");
 
 	err_code failure = parse_link_stream(&lks, fgraph, fattack );
 	def_err_handler(failure, "main parse_link", failure);
@@ -51,10 +56,10 @@ int main(int argc, char ** argv) {
 	failure = temporal_floyd_warshall(&lks, &dma);
 	def_err_handler(failure, "main temporal_floyd_warshall", failure);
 	//sums every little square in the matrix and writes results
-	uint64_t ret ; 
+	double ret ; 
 	failure = sum_dma(&ret,  &dma);
 	def_err_handler(failure, "main sum_dma", failure);
-	fprintf(flux, "%lu\n", ret);
+	fprintf(flux, "sum = %.2f\n", ret);
 	//cleans stuff up
 	if(flux != stdout ){
 		fclose(flux);
