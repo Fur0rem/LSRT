4680077fe018d926a884b93a041bfd4a96e95856
Dassire <damienassire@gmail.com>
2024-02-16 13:46:53 +0100

    parameter delta for the write_to_file function

--- scripts/CityGraph.py
+++ scripts/CityGraph.py
@@ -114,7 +114,7 @@ class CityGraph :
 		"""Accesses an edge through its index"""
 		return list(self.graph.edges())[edge_idx]
 
-	def write_to_file(self, file_path : str, delta=1/3600) -> None : # by default, delta convert the length to have the speed correzsponding to m/s
+	def write_to_file(self, file_path : str, delta=1) -> None : # by default, delta convert the length to have the speed correzsponding to m/s
 		nodeList=list(self.graph.nodes); # Should be changed
 		csr=nx.to_scipy_sparse_array(self.graph, format='csr'); # format=csr not needed
 		V=np.ndarray(len(csr.indices), dtype=np.uint32)
@@ -130,7 +130,7 @@ class CityGraph :
 					if "weight" in e.keys():
 						weights.append(e["weight"])
 					elif "maxspeed" in e.keys():
-						weights.append(int(e["length"]*delta)/cast_tmp(e["maxspeed"])); # TODO Remove this cast
+						weights.append(int(e["length"]*delta)/(cast_tmp(e["maxspeed"])*3600)); # TODO Remove this cast
 					else:
 						d.append(e["length"])
 				if(len(weights)==0):
@@ -164,7 +164,7 @@ class CityGraph :
 
 		f=open(file_path, "w");
 		# f.write("%d %d %d\n".format(len(V), len(csr.indices), len(csr.indptr)));
-		f.write(" ".join([str(x) for x in (len(V), len(csr.indices), len(csr.indptr))])+"\n") # TODO change this line
+		f.write(" ".join([str(x) for x in (len(V), len(csr.indices), len(csr.indptr), delta)])+"\n") # TODO change this line
 		for i in V:
 			f.write(str(i)+" ");
 		f.write("\n");
